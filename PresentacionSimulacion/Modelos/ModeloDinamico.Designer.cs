0ce1119e5bd97b25ff590a4daaa20516b3cb68cc
balta <balta@DESKTOP-RITKHCU>
2023-02-24 12:38:21 -0800

    Prueba de isai

--- PresentacionSimulacion/Modelos/ModeloDinamico.Designer.cs
+++ PresentacionSimulacion/Modelos/ModeloDinamico.Designer.cs
@@ -35,6 +35,7 @@
             this.button4 = new System.Windows.Forms.Button();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnSiguiente
@@ -104,11 +105,23 @@
             this.label1.TabIndex = 67;
             this.label1.Text = "Modelo Dinamico";
             // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.button1.Font = new System.Drawing.Font("Ink Free", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(324, 202);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(153, 46);
+            this.button1.TabIndex = 73;
+            this.button1.Text = "Ver Ejemplo";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
             // ModeloDinamico
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.btnSiguiente);
             this.Controls.Add(this.btnSalir);
             this.Controls.Add(this.btnMenu);
@@ -129,5 +142,6 @@
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
     }
 }
